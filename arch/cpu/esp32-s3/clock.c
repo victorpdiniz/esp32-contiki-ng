de6be80aba91cfc7608a9b4773a547036e857ca6
victorpdiniz <vpd@cin.ufpe.br>
2025-12-19 15:35:35 -0300

    docs: deleting a few README.md files.

--- arch/cpu/esp32-s3/clock.c
+++ arch/cpu/esp32-s3/clock.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Contiki-NG ESP32-S3 Port
+ * Copyright (c) 2025, Victor
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
